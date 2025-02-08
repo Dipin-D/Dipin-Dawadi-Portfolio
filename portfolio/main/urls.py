552465e59fcfe9d3a56f0eae1e06e4ff339ab9e3
Dipin-D <dipindawadi@gmail.com>
2025-02-08 16:39:57 -0600

    small

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -1,10 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
-
 
 urlpatterns = [
     path('', views.home, name='home'),  
     path('experiences/', views.experiences, name='experiences'), 
     path('projects/', views.projects, name='projects'), 
-
 ]
+
+if not settings.DEBUG:  # Only in production
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
