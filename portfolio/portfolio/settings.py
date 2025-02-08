a02f5d928176cd8551f63d0bbfc5fd4cf42756ea
Dipin-D <dipindawadi@gmail.com>
2025-02-08 15:19:01 -0600

    static changes

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +122,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Static files configuration
+# settings.py
+
+# Add this if your static files are NOT inside an app’s static/ directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  
+]
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
